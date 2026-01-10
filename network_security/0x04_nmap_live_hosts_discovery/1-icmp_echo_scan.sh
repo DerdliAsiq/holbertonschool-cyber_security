c75e38b7df4ed1e45178b6f8e83aa9fc5cb52e11
DerdliAsiq <120935016+DerdliAsiq@users.noreply.github.com>
2026-01-10 22:38:34 +0000

    Update 1-icmp_echo_scan.sh

--- network_security/0x04_nmap_live_hosts_discovery/1-icmp_echo_scan.sh
+++ network_security/0x04_nmap_live_hosts_discovery/1-icmp_echo_scan.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-# Nmap kullanarak ICMP Echo (Ping) taraması yapan betik
-# -sn: Port taramasını atla (sadece host discovery)
-# -PE: ICMP Echo Request paketlerini kullan
 sudo nmap -sn -PE "$1"
