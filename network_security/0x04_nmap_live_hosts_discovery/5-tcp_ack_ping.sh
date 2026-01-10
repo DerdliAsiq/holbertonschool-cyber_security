5fed0dab4b96466c508a35433ce4ed9398dad7ab
DerdliAsiq <120935016+DerdliAsiq@users.noreply.github.com>
2026-01-10 22:39:25 +0000

    Update 5-tcp_ack_ping.sh

--- network_security/0x04_nmap_live_hosts_discovery/5-tcp_ack_ping.sh
+++ network_security/0x04_nmap_live_hosts_discovery/5-tcp_ack_ping.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-# Nmap kullanarak TCP ACK Ping taraması yapan betik
-# -sn: Host keşfi sonrası port taraması yapmaz
-# -PA22,80,443: Belirtilen portlar üzerinden TCP ACK paketleri gönderir
 sudo nmap -sn -PA22,80,443 "$1"
