03711499b74806da6b7a2fd069e066e1053e045d
DerdliAsiq <120935016+DerdliAsiq@users.noreply.github.com>
2026-01-10 22:38:48 +0000

    Update 2-icmp_timestamp_scan.sh

--- network_security/0x04_nmap_live_hosts_discovery/2-icmp_timestamp_scan.sh
+++ network_security/0x04_nmap_live_hosts_discovery/2-icmp_timestamp_scan.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-# Nmap kullanarak ICMP Timestamp taraması yapan betik
-# -sn: Port taramasını devre dışı bırakır
-# -PP: ICMP Timestamp Request paketlerini kullanır
 sudo nmap -sn -PP "$1"
