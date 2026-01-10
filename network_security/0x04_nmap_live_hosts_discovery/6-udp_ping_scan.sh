a26145946b30810f1c8a8355d634ee4000fae799
DerdliAsiq <120935016+DerdliAsiq@users.noreply.github.com>
2026-01-10 22:39:38 +0000

    Update 6-udp_ping_scan.sh

--- network_security/0x04_nmap_live_hosts_discovery/6-udp_ping_scan.sh
+++ network_security/0x04_nmap_live_hosts_discovery/6-udp_ping_scan.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-# Nmap kullanarak UDP Ping taraması yapan betik
-# -sn: Host keşfi sonrası port taraması yapmaz
-# -PU53,161,162: Belirtilen portlar üzerinden UDP paketleri gönderir
 sudo nmap -sn -PU53,161,162 "$1"
