371c70359ce4b3593a72815367b95fb6fd7ce250
DerdliAsiq <120935016+DerdliAsiq@users.noreply.github.com>
2026-01-10 22:37:41 +0000

    Update 0-arp_scan.sh

--- network_security/0x04_nmap_live_hosts_discovery/0-arp_scan.sh
+++ network_security/0x04_nmap_live_hosts_discovery/0-arp_scan.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-# Nmap kullanarak ARP taraması yapan betik
-# -sn: Port taramasını devre dışı bırakır (Host Discovery only)
-# -PR: ARP taraması (ARP Ping) yapılmasını sağlar
-sudo nmap -sn -PR "$1"
+sudo nmap -sn -PR $1
