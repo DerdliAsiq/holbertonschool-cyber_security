bbd6266e5a8f641458fce22f442995ea9fab869e
DerdliAsiq <kanlisilah@gmail.com>
2026-04-14 16:31:54 -0400

    Optimize Task 3: Targeted filtering for root account SSH breaches

--- web_application_security/0x0c_web_application_foresics/3-ips.sh
+++ web_application_security/0x0c_web_application_foresics/3-ips.sh
@@ -1,11 +1,2 @@
 #!/bin/bash
-awk '
-/Failed/ {for(i=1;i<=NF;i++) if($i=="from") f[$(i+1)]=1}
-/Accepted/ {for(i=1;i<=NF;i++) if($i=="from") a[$(i+1)]=1}
-END {
-    count=0;
-    for (ip in a) {
-        if (ip in f) count++;
-    }
-    print count
-}' "${1:-auth.log}"
+grep "Accepted password for root" "${1:-auth.log}" | awk '{print $11}' | sort -u | wc -l
