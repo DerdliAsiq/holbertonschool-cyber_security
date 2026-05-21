6e897b2ef97e507ed3808702c3f30b51552644d6
DerdliAsiq <kanlisilah@gmail.com>
2026-05-21 12:02:09 +0400

    SELAM BABY

---
+++ scripting_cyber/0x00-ruby_scripting/10-password_cracked.rb
@@ -0,0 +1,30 @@
+#!/usr/bin/env ruby
+require 'digest'
+
+if ARGV.length != 2
+  puts "Usage: 10-password_cracked.rb HASHED_PASSWORD DICTIONARY_FILE"
+  exit
+end
+
+target_hash = ARGV[0]
+dictionary_path = ARGV[1]
+
+unless File.exist?(dictionary_path)
+  puts "Dictionary file not found."
+  exit
+end
+
+found = false
+
+File.open(dictionary_path, 'r').each_line do |line|
+  word = line.strip
+  if Digest::SHA256.hexdigest(word) == target_hash
+    puts "Password found: #{word}"
+    found = true
+    break
+  end
+end
+
+unless found
+  puts "Password not found in dictionary."
+end
