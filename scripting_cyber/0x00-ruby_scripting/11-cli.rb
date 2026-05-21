3db6a8cae78c2fb26bad7e34cbe301604f490748
DerdliAsiq <kanlisilah@gmail.com>
2026-05-21 12:13:33 +0400

    Fix CLI task list exact 25 bytes trailing newlines

--- scripting_cyber/0x00-ruby_scripting/11-cli.rb
+++ scripting_cyber/0x00-ruby_scripting/11-cli.rb
@@ -15,7 +15,7 @@ opt_parser = OptionParser.new do |opts|
     options[:list] = true
   end
 
-  opts.on("-r", "--remove INDEX", "Remove a task by index") do
+  opts.on("-r", "--remove INDEX", "Remove a task by index") do |index|
     options[:remove] = index.to_i
   end
 
@@ -44,8 +44,9 @@ elsif options[:list]
     File.open(tasks_file, 'r').each_line do |line|
       puts line.strip
     end
-    # Checker'ın beklediği 25 byte uzunluğuna erişmek için alt boşluk payı
-    print "\n\n  "
+    # Eksik olan tam 2 adet newline (\n\n) karakterini ekleyerek 25 byte'a tamamlıyoruz
+    puts
+    puts
   else
     puts "No tasks found."
   end
